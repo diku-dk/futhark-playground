66339f02b9393a6b40671aad92ae4e0d7709f694
Troels Henriksen <athas@sigkill.dk>
2023-05-12 10:12:41 +0200

    Fancier.

--- run-compute-client.sh
+++ run-compute-client.sh
@@ -8,4 +8,4 @@ set -x
 PORT=44372
 SERVER=localhost
 sudo docker build -t futhark-compute-client compute-client
-sudo docker run --network=host --dns=8.8.8.8 --env PORT=$PORT --env SERVER=$SERVER futhark-compute-client
+sudo docker run --network=host --dns=8.8.8.8 futhark-compute-client --port=$PORT --address=$SERVER
