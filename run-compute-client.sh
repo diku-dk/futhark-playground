e574462628f2299b050b4424b1044fc7ef8c00ee
Troels Henriksen <athas@sigkill.dk>
2023-05-12 10:05:57 +0200

    Fix script.

--- run-compute-client.sh
+++ run-compute-client.sh
@@ -2,7 +2,10 @@
 #
 # Build a compute client Docker image and run it.
 
+set -e
+set -x
+
 PORT=44372
 SERVER=localhost
-sudo docker build -t futhark-playground-client playground-client
-sudo docker run --network=host --dns=8.8.8.8 --env PORT=$PORT --env SERVER=$SERVER futhark-playground-client
+sudo docker build -t futhark-compute-client compute-client
+sudo docker run --network=host --dns=8.8.8.8 --env PORT=$PORT --env SERVER=$SERVER futhark-compute-client
